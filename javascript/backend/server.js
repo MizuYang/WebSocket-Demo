b6ed9d97758bda28d932191b3892eecec4292210
Mizu <psdk55052004@gmail.com>
2024-09-04 15:31:15 +0800

    新增 後端 server.js

---
+++ javascript/backend/server.js
@@ -0,0 +1,40 @@
+// import library
+const express = require('express')
+const ServerSocket = require('ws').Server   // 引用 Server
+
+// 指定一個 port
+const PORT = 8080
+
+// 建立 express 物件並用來監聽 8080 port
+const server = express()
+    .listen(PORT, () => console.log(`[Server] Listening on https://localhost:${PORT}`))
+
+// 建立實體，透過 ServerSocket 開啟 WebSocket 的服務
+const wss = new ServerSocket({ server })
+
+wss.on('connection', (ws, req) => {
+  // 給每個連線的客戶端一個獨立的 id
+  const id = req.headers['sec-websocket-key']
+  console.log('[後端] - 連線成功', id)
+
+  ws.on('message', msg => {
+
+    let clients = wss.clients
+    // 使用循環向每個客戶端發送訊息
+    clients.forEach(client => {
+      client.send(`${ws.id}: ` + msg)
+    })
+
+    // console.log('[後端] - 收到訊息: ', msg)
+    // ws.send('[後端] 傳送訊息給前端')
+    // ws.send({
+    //   message: '[後端] - 傳送訊息給前端',
+    //   msg
+    // })
+
+  })
+
+  ws.on('close', () => {
+      console.log('[後端] - 連線中斷')
+  })
+})
